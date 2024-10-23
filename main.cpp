95b716d71b582cc0dc2ef3451c980fb82a064762
Omar Azzaoui <56740409+pickleomar@users.noreply.github.com>
2024-10-23 22:00:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@ private:
     float vitesse;
 public:
 // Constructeur par défaut
-Voiture() : marque("Default"), modele("Default"), annee(0), kilometrage(0.0), vitesse(0.0) {}
+Voiture(): marque("Default"), modele("Default"), annee(0), kilometrage(0.0), vitesse(0.0){}
 
 // Constructeur avec paramètres
 Voiture(string marque1, string modele1, int anne1, float km1, float vitesse1):
-marque(marque1), modele(modele1), annee(anne1), kilometrage(km1), vitesse(vitesse1) {}
+marque(marque1), modele(modele1), annee(anne1), kilometrage(km1), vitesse(vitesse1){}
 
 // Méthode accelerer
 void accelerer(float valeur) {
@@ -24,7 +24,7 @@ void accelerer(float valeur) {
 // Méthode freiner
 void freiner(float valeur) {
     if (vitesse-valeur < 0)
-        vitesse = 0;
+        vitesse= 0;
     else
         vitesse-= valeur;
 }
@@ -45,5 +45,10 @@ void avancer(float distance) {
 }
 };
 int main(){
-return 0;
+    Voiture voiture1("Lincoln", "Continental", 2024, 10000, 240);
+    voiture1.accelerer(20);
+    voiture1.avancer(150);
+    voiture1.freiner(50);
+    voiture1.afficherInfo();
+    return 0;
 }
